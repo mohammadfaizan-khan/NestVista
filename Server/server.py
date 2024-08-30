ba54eee7fe1e36eda8feab5b4a2afc6c53847ad2
mohammadfaizan-khan <123552129+mohammadfaizan-khan@users.noreply.github.com>
2024-08-30 23:57:10 +0530

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -3,16 +3,7 @@ import util
 
 app = Flask(__name__)
 
-@app.route('/get_location_names', methods=['GET'])
-def get_location_names():
-    response = jsonify({
-        'locations': util.get_location_names()
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
     location = request.form['location']
     sqft = float(request.form['total_sqft'])
@@ -21,13 +12,18 @@ def predict_home_price():
     bhk = int(request.form['bhk'])
     
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,sqft,bath,balcony,bhk)
+        'estimated_price': util.get_estimated_price(location, sqft, bath, balcony, bhk)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
     app.run()
